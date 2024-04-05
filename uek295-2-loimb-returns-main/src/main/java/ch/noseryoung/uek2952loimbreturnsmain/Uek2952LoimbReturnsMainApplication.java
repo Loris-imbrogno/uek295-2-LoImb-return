3577855c48624d7f6b3cb77c296769407a3c7b2b
loris imbrogno <143715480+Loris-imbrogno@users.noreply.github.com>
2024-04-05 11:03:06 +0200

    created documents structure with springinitalizr and added the dependencies

---
+++ uek295-2-loimb-returns-main/src/main/java/ch/noseryoung/uek2952loimbreturnsmain/Uek2952LoimbReturnsMainApplication.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.uek2952loimbreturnsmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uek2952LoimbReturnsMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uek2952LoimbReturnsMainApplication.class, args);
+	}
+
+}
---
+++ uek295-2-loimb-returns-main/src/test/java/ch/noseryoung/uek2952loimbreturnsmain/Uek2952LoimbReturnsMainApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.uek2952loimbreturnsmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uek2952LoimbReturnsMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
